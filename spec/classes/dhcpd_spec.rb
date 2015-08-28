73ff2defc4cb3fb99e54529a7978bd0a7c717606
Nick Miller <nick.miller@onyxpoint.com>
2015-08-28 14:13:36 -0400

    (SIMP-399) Add link to JIRA and the wiki in README
    Updated the module metadata to align with the skeleton
    and updated the spec tests.
    
    SIMP-399 #comment added dhcp
    Change-Id: Ia530a414882e188d69f69ba108fef8cb4f65acac

--- spec/classes/dhcpd_spec.rb
+++ spec/classes/dhcpd_spec.rb
@@ -2,30 +2,41 @@ require 'spec_helper'
 
 describe 'dhcp::dhcpd' do
 
-  it { should create_class('dhcp::dhcpd') }
-  it { should compile.with_all_deps }
+  context 'supported operating systems' do
+    on_supported_os.each do |os, facts|
+      let(:facts) do
+        facts
+      end
 
-  it { should create_file('/etc/dhcp').with_ensure('directory') }
-  it { should create_file('/etc/dhcp/dhcpd.conf').with({
-      :ensure  => 'file',
-      :notify  => 'Rsync[dhcpd]',
-      :require => 'File[/etc/dhcp]'
-    })
-  }
-
-  it { should create_file('/etc/dhcpd.conf').with({
-      :ensure => 'symlink',
-      :target => '/etc/dhcp/dhcpd.conf'
-    })
-  }
-
-  it { should create_iptables_rule('allow_bootp') }
-
-  it { should contain_package('dhcp').with_ensure('latest') }
-
-  it { should contain_service('dhcpd').with({
-      :ensure  =>'running',
-      :require => ['File[/etc/dhcpd.conf]', 'Package[dhcp]']
-    })
-  }
+      context "on #{os}" do
+        it { should create_class('dhcp::dhcpd') }
+        it { should compile.with_all_deps }
+      
+        it { should create_file('/etc/dhcp').with_ensure('directory') }
+        it { should create_file('/etc/dhcp/dhcpd.conf').with({
+            :ensure  => 'file',
+            :notify  => 'Rsync[dhcpd]',
+            :require => 'File[/etc/dhcp]'
+          })
+        }
+      
+        it { should create_file('/etc/dhcpd.conf').with({
+            :ensure => 'symlink',
+            :target => '/etc/dhcp/dhcpd.conf'
+          })
+        }
+      
+        it { should create_iptables_rule('allow_bootp') }
+      
+        it { should contain_package('dhcp').with_ensure('latest') }
+      
+        it { should contain_service('dhcpd').with({
+            :ensure  =>'running',
+            :require => ['File[/etc/dhcpd.conf]', 'Package[dhcp]']
+          })
+        }
+      end
+    end
+  end
 end
+
--- spec/classes/init_spec.rb
+++ spec/classes/init_spec.rb
@@ -1,11 +1,16 @@
-require 'spec_helper'
-
 describe 'dhcp' do
 
-  it { should create_class('dhcp') }
+  context 'supported operating systems' do
+    on_supported_os.each do |os, facts|
+      let(:facts) do
+        facts
+      end
 
-  context 'base' do
-    it { should compile.with_all_deps }
-    it { should contain_class('dhcp::dhcpd') }
+      context "on #{os}" do
+        it { should compile.with_all_deps }
+        it { should contain_class('dhcp::dhcpd') }
+      end
+    end
   end
 end
+
