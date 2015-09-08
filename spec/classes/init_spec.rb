e5ac59baaccd8f15206c81413d4d8fc8559d46c4
nick-markowski <nmarkowski@keywcorp.com>
2015-09-08 13:18:56 +0000

    (SIMP-438) Update pupmod-simp-dhcp for skeleton
    - Easiest way to make travis tests pass.
    - Updated README.md
    
    SIMP-399 #comment added dhcp readme
    SIMP-438 #close Update dhcp for skeleton.
    Change-Id: Ie63bda7b3abc50d1710de6777c0eb5f5d4b9a65d

--- spec/classes/init_spec.rb
+++ spec/classes/init_spec.rb
@@ -1,11 +1,20 @@
-require 'spec_helper'
-
 describe 'dhcp' do
 
-  it { should create_class('dhcp') }
+  context 'supported operating systems' do
+    on_supported_os.each do |os, facts|
+      let(:facts) do
+        facts
+      end
 
-  context 'base' do
-    it { should compile.with_all_deps }
-    it { should contain_class('dhcp::dhcpd') }
+      context "on #{os}" do
+        context 'in the :foo environment' do
+          let(:environment){:foo}
+
+          it { should compile.with_all_deps }
+          it { should contain_class('dhcp::dhcpd') }
+        end
+      end
+    end
   end
 end
+
